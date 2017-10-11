353d5f353fd03252d9b5692c78132483ce389433
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-10-11 18:38:47 +0200

    [TASK] Implement dist download proxy

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Provider;
 use App\Models\Repository;
+use Composer\Semver\VersionParser;
+use GuzzleHttp\Client as HttpClient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -11,7 +13,90 @@ class RepositoryController extends Controller
 {
 	public function downloadDist(string $repoName, string $namespace, string $package, string $version, string $reference, string $type)
 	{
-		return response('WIP', 500);
+		// Find the repository
+		$repo = Repository::where('name', $repoName)
+			->first();
+		if ($repo === null) {
+			return response('Repository not found', 404);
+		}
+
+		// Locate the provider file
+		$packageName = sprintf('%s/%s', $namespace, $package);
+		$providerPath = sprintf('%s/repo/%s/%s.json', public_path(), $repo->name, $packageName);
+		if (!is_readable($providerPath)) {
+			return response('Unknown provider', 404);
+		}
+
+		// Parse provider data
+		$providerData = json_decode(file_get_contents($providerPath));
+
+		// Find data for the requested package
+		if (!isset($providerData->packages->{$packageName})) {
+			throw new \Exception('Missing package in provider file');
+		}
+		$packageData = $providerData->packages->{$packageName};
+
+		// Iterate over available versions and select one with a matching version property
+		$matchingVersion = null;
+		$versionParser = new VersionParser();
+		foreach ($packageData as $versionKey => $versionData) {
+			// Obtain normalized version
+			if (isset($versionData->version_normalized)) {
+				$currentVersion = $versionData->version_normalized;
+			} else if (isset($versionData->version)) {
+				$currentVersion = $versionParser->normalize($versionData->version);
+			} else {
+				Log::warning("No version information available for $packageName at key $versionKey");
+				continue;
+			}
+
+			// Is this a match?
+			if ($currentVersion === $version) {
+				$matchingVersion = $versionData;
+				break;
+			}
+		}
+		if ($matchingVersion === null) {
+			return response('Unknown version', 404);
+		}
+
+		// Confirm that dist info matches requested reference and type
+		$distData = $matchingVersion->dist;
+		if (($distData->reference !== $reference) || ($distData->type !== $type)) {
+			return response('Unknown dist for this version', 404);
+		}
+
+		// Prepare download
+		$distURL = $distData->url;
+		$distLocalPath = sprintf('%s/repo/%s/dist/%s/%s/%s.%s', public_path(), $repo->name, $packageName, $version, $reference, $type);
+		$distLocalDir = dirname($distLocalPath);
+		if (!is_dir($distLocalDir)) {
+			mkdir($distLocalDir, 0750, true);
+		}
+
+		// Download dist and stream to output immediately
+		$storeAndOutput = function() use ($distURL, $distLocalPath) {
+			$client = new HttpClient();
+			$response = $client->request('GET', $distURL, ['stream' => true]);
+			$body = $response->getBody();
+
+			// TODO: write to temporary file and rename afterwards
+
+			$fpOut = fopen($distLocalPath, 'wb');
+			try {
+				while (!$body->eof()) {
+					$data = $body->read(8192);
+					fwrite($fpOut, $data);
+					echo $data;
+				}
+			} finally {
+				fclose($fpOut);
+			}
+		};
+		return response()
+			->stream($storeAndOutput, 200, [
+				'Content-Type' => 'application/zip',
+			]);
 	}
 
 	public function downloadPackage(string $repoName, string $namespace, string $package)
