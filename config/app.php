83d30f68cfeb746e7a7bdee73207d0817ce2626b
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-10-12 11:21:36 +0200

    [TASK] Use info log level if unconfigured in env

--- config/app.php
+++ config/app.php
@@ -123,7 +123,7 @@ return [
 
     'log' => env('APP_LOG', 'single'),
 
-    'log_level' => env('APP_LOG_LEVEL', 'debug'),
+    'log_level' => env('APP_LOG_LEVEL', 'info'),
 
     /*
     |--------------------------------------------------------------------------
