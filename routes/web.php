73b188ce2b702a3d17f5786deb6ef0d8023220c4
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2017-10-25 15:01:36 +0200

    [TASK] Add endpoints metadata resource

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/dist/{site}/{path}', 'DistController@download')
     ->where(['path' => '.*']);
 Route::get('/repo/{repo}/packages.json', 'RepositoryController@getPackages');
 Route::get('/repo/{repo}/packages-velocita.json', 'RepositoryController@getPackagesForVelocita');
 Route::get('/repo/{repo}/{namespace}/{package}.json', 'RepositoryController@downloadPackage');
+Route::get('/endpoints', 'MetadataController@getEndpoints');
