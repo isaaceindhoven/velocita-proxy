fae5eba44dfa075a1b920927067d52137fd2e28f
Jelle Raaijmakers <jelle@gmta.nl>
2021-10-19 10:32:42 +0200

    Add a rudimentary index.html

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/ash
 set -eu
 
-# Generate proxy sites configuration
-erb -U -T '-' /usr/local/lib/velocita/proxy.conf.erb > /etc/nginx/conf.d/proxy.conf
+function renderTemplate() {
+    local source=$1
+    local target=$2
+    erb -U -T '-' "${source}" > "${target}"
+}
 
-# Generate mirror configuration
+renderTemplate /usr/local/lib/velocita/proxy.conf.erb /etc/nginx/conf.d/proxy.conf
+
 mkdir -p /var/www/html
-erb -U -T '-' /usr/local/lib/velocita/mirrors.json.erb > /var/www/html/mirrors.json
+renderTemplate /usr/local/lib/velocita/index.html.erb /var/www/html/index.html
+renderTemplate /usr/local/lib/velocita/mirrors.json.erb /var/www/html/mirrors.json
 
 exec "$@"
